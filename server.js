f9eec06ed7e7c86b03713f8a4acb5ebc586122a2
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-09-10 00:46:04 +0530

    modify package.json

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.get('/', function(req, res) {
     res.send('Hello from NodeJS  at '+ new Date());
 });
- 
- 
+
 app.listen(8080, ip);
  
 module.exports = app;
