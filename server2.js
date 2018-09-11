7cd3c172c258b4a969333b517a377ff50d056e36
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-09-11 13:09:04 +0530

    node and mongo

---
+++ server2.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/db1';
+
+var str = "";
+
+app.route('/Employeeid').get(function(req, res) {
+   console.log("method called...");	
+   MongoClient.connect(url, function(err, db) {
+       var collection = db.collection('Employee');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Employee id  " + item.Employeeid + "</br>";
+           }
+       }, function(err) {
+           res.send(str);
+           db.close();
+          }
+       );
+   });
+});
+var server = app.listen(8080, function() {});
