13fba1349e86ad5b8dcc2996a89889046db1fc33
vijay limbasiya <vijay.limbasiya@netcracker.com>
2018-09-11 13:17:37 +0530

    sample db

--- server2.js
+++ server2.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/db1';
+var url = 'mongodb://localhost:27017/sampledb';
 
 var str = "";
 
